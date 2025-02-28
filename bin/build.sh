ee413d0229168c3108ee71c0f1a1f9b1901c4c86
Joaquin Colacci <joaquincolacci@gmail.com>
2025-02-28 08:49:30 +0100

    Fix build

--- bin/build.sh
+++ bin/build.sh
@@ -7,17 +7,13 @@ cd external/psql
 ./configure --with-openssl
 make clean
 make -C src/interfaces/libpq
-/sudo make install -C src/interfaces/libpq
+make install -C src/interfaces/libpq
 make -C src/common
-sudo make install -C src/common
-sudo make install -C src/port
+make install -C src/common
+make install -C src/port
 
 cd ../..
 
 cargo build --release
 make -C external/psql/src/bin/psql
-sudo make install -C external/psql/src/bin/psql
-
-cp target/release/libpsqlx_ai.so ~/.local/share/psqlx/plugins/
-
-/usr/local/pgsql/bin/psqlx "postgresql://BenchmarkRole:8MGUYwHcu6Da@ep-lively-tooth-a49kswzq-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+make install -C external/psql/src/bin/psql
